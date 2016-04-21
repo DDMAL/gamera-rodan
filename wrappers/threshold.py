c831125f906e15b969801ea82dd78996076b1178
Michael Ryan Bannon <ryan.bannon@gmail.com>
2016-04-21 15:31:52 -0400

    FEATURE: basic gamera threshold algs

--- wrappers/threshold.py
+++ wrappers/threshold.py
@@ -77,3 +77,221 @@ class gamera_otsu_threshold(RodanTask):
     	image_result = image_source.otsu_threshold(compression) 
     	image_result.save_PNG(outputs['Onebit PNG image'][0]['resource_path'])
         return True
+
+class gamera_tsai_moment_preserving_threshold(RodanTask):
+
+    name = 'Tsai Moment Preserving Threshold'
+    author = 'Ryan Bannon'
+    description = gamera.plugins.threshold.tsai_moment_preserving_threshold.escape_docstring().replace("\\n", "\n").replace('\\"', '"')
+    settings = {
+        'title': 'Tsai Moment Preserving Threshold',
+        'type': 'object',
+        'properties': {
+            'Storage format': {
+                'enum': ['Dense (no compression)', 'RLE (run-length encoding compression)'],
+                'type': 'string',
+                'default': 'Dense (no compression)',
+                'description': 'Specifies the compression type for the result.'
+            }
+        }
+    }
+
+    enabled = True
+    category = "Gamera - Threshold"
+    interactive = False
+
+    input_port_types = [{
+        'name': 'Greyscale PNG image',
+        'resource_types': ['image/greyscale+png'],
+        'minimum': 1,
+        'maximum': 1
+    }]
+    output_port_types = [{
+        'name': 'Onebit PNG image',
+        'resource_types': ['image/onebit+png'],
+        'minimum': 1,
+        'maximum': 1
+    }]
+
+    def run_my_task(self, inputs, settings, outputs):
+
+        # Set execution settings.
+        compression = 0
+        if settings['Storage format'] == 'RLE (run-length encoding compression)':
+            compression = 1
+
+        image_source = load_image(inputs['Greyscale PNG image'][0]['resource_path'])
+        image_result = image_source.tsai_moment_preserving_threshold(compression) 
+        image_result.save_PNG(outputs['Onebit PNG image'][0]['resource_path'])
+        return True
+
+class gamera_abutaleb_threshold(RodanTask):
+
+    name = 'Abutaleb locally-adaptive threshold'
+    author = 'Ryan Bannon'
+    description = gamera.plugins.threshold.abutaleb_threshold.escape_docstring().replace("\\n", "\n").replace('\\"', '"')
+    settings = {
+        'title': 'Abutaleb locally-adaptive threshold',
+        'type': 'object',
+        'properties': {
+            'Storage format': {
+                'enum': ['Dense (no compression)', 'RLE (run-length encoding compression)'],
+                'type': 'string',
+                'default': 'Dense (no compression)',
+                'description': 'Specifies the compression type for the result.'
+            }
+        }
+    }
+
+    enabled = True
+    category = "Gamera - Threshold"
+    interactive = False
+
+    input_port_types = [{
+        'name': 'Greyscale PNG image',
+        'resource_types': ['image/greyscale+png'],
+        'minimum': 1,
+        'maximum': 1
+    }]
+    output_port_types = [{
+        'name': 'Onebit PNG image',
+        'resource_types': ['image/onebit+png'],
+        'minimum': 1,
+        'maximum': 1
+    }]
+
+    def run_my_task(self, inputs, settings, outputs):
+
+        # Set execution settings.
+        compression = 0
+        if settings['Storage format'] == 'RLE (run-length encoding compression)':
+            compression = 1
+
+        image_source = load_image(inputs['Greyscale PNG image'][0]['resource_path'])
+        image_result = image_source.abutaleb_threshold(compression) 
+        image_result.save_PNG(outputs['Onebit PNG image'][0]['resource_path'])
+        return True
+
+class gamera_bernsen_threshold(RodanTask):
+
+    name = 'Bernsen threshold'
+    author = 'Ryan Bannon'
+    description = gamera.plugins.threshold.bernsen_threshold.escape_docstring().replace("\\n", "\n").replace('\\"', '"')
+    settings = {
+        'title': 'Bernsen threshold',
+        'type': 'object',
+        'properties': {
+            'Storage format': {
+                'enum': ['Dense (no compression)', 'RLE (run-length encoding compression)'],
+                'type': 'string',
+                'default': 'Dense (no compression)',
+                'description': 'Specifies the compression type for the result.'
+            },
+            'Region size': {
+                'type': 'integer',
+                'minimum': 1,
+                'maximum': 50,
+                'default': 11,
+                'description': 'The size of each region in which to calculate a threshold.'
+            },
+            'Contrast limit': {
+                'type': 'integer',
+                'minimum': 0,
+                'maximum': 255,
+                'default': 80,
+                'description': 'The minimum amount of contrast required to threshold.'
+            },
+            'Doubt to black': {
+                'type': 'boolean',
+                'default': False,
+                'description': 'When True, ''doubtful'' values are set to black, otherwise to white.'
+            }
+        }
+    }
+
+    enabled = True
+    category = "Gamera - Threshold"
+    interactive = False
+
+    input_port_types = [{
+        'name': 'Greyscale PNG image',
+        'resource_types': ['image/greyscale+png'],
+        'minimum': 1,
+        'maximum': 1
+    }]
+    output_port_types = [{
+        'name': 'Onebit PNG image',
+        'resource_types': ['image/onebit+png'],
+        'minimum': 1,
+        'maximum': 1
+    }]
+
+    def run_my_task(self, inputs, settings, outputs):
+
+        image_source = load_image(inputs['Greyscale PNG image'][0]['resource_path'])
+        image_result = image_source.bernsen_threshold(settings['Storage format'], settings['Region size'], settings['Contrast limit'], settings['Doubt to black']) 
+        image_result.save_PNG(outputs['Onebit PNG image'][0]['resource_path'])
+        return True
+
+class gamera_djbu_threshold(RodanTask):
+
+    name = 'DjVu threshold'
+    author = 'Ryan Bannon'
+    description = gamera.plugins.threshold.djvu_threshold.escape_docstring().replace("\\n", "\n").replace('\\"', '"')
+    settings = {
+        'title': 'DjVu threshold',
+        'type': 'object',
+        'properties': {
+            'Smoothness': {
+                'type': 'number',
+                'minimum': 0.0,
+                'maximum': 1.0,
+                'default': 0.2,
+                'description': 'The amount of effect that parent blocks have on their children blocks. Higher values will result in more smoothness between blocks.  Expressed as a percentage between 0.0 and 1.0.'
+            },
+            'Maximum block size': {
+                'type': 'integer',
+                'minimum': 1,
+                'default': 512,
+                'description': 'The size of the largest block to determine a threshold.'
+            },
+            'Minimum block size': {
+                'type': 'integer',
+                'minimum': 1,
+                'default': 64,
+                'description': 'The size of the smallest block to determine a threshold.'
+            },
+            'Block factor': {
+                'type': 'integer',
+                'minimum': 1,
+                'maximum': 8,
+                'default': 2,
+                'description': 'The number of child blocks (in each direction) per parent block. For instance, a *block_factor* of 2 results in 4 children per parent.'
+            }
+        }
+    }
+
+    enabled = True
+    category = "Gamera - Threshold"
+    interactive = False
+
+    input_port_types = [{
+        'name': 'RGB PNG image',
+        'resource_types': ['image/rgb+png'],
+        'minimum': 1,
+        'maximum': 1
+    }]
+    output_port_types = [{
+        'name': 'Onebit PNG image',
+        'resource_types': ['image/onebit+png'],
+        'minimum': 1,
+        'maximum': 1
+    }]
+
+    def run_my_task(self, inputs, settings, outputs):
+
+        image_source = load_image(inputs['RGB PNG image'][0]['resource_path'])
+        image_result = image_source.djvu_threshold(settings['Smoothness'], settings['Maximum block size'], settings['Minimum block size'], settings['Block factor']) 
+        image_result.save_PNG(outputs['Onebit PNG image'][0]['resource_path'])
+        return True
+
