9bbaf7c968f5b2a49ee08f165b1ebaed716ca0dd
AFFogarty <andrew.f.fogarty@gmail.com>
2016-05-13 14:42:10 -0400

    New: Better port names on CC Analysis

--- wrappers/plugins/cc_analysis.py
+++ wrappers/plugins/cc_analysis.py
@@ -40,23 +40,23 @@ class CCAnalysis(RodanTask):
     interactive = False
     settings = {}
     input_port_types = [{
-        'name': 'Staffless Image',
+        'name': '1-Bit PNG Image',
         'resource_types': ['image/onebit+png'],
         'minimum': 1,
         'maximum': 1
     }]
     output_port_types = [{
-        'name': 'Connected Components',
+        'name': 'GameraXML - Connected Components',
         'resource_types': ['application/gamera+xml'],
         'minimum': 1,
         'maximum': 1
     }]
 
     def run_my_task(self, inputs, settings, outputs):
-        image_path = inputs['Staffless Image'][0]['resource_path']
+        image_path = inputs['1-Bit Image'][0]['resource_path']
         input_image = gamera.core.load_image(image_path)
         ccs = input_image.cc_analysis()
         output_xml = gamera.gamera_xml.WriteXMLFile(glyphs=ccs,
                                                     with_features=True)
         output_xml.write_filename(
-            outputs['Connected Components'][0]['resource_path'])
+            outputs['GameraXML - Connected Components'][0]['resource_path'])
