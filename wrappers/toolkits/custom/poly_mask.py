24fa56bd4e603709c34877eb8a28120a394e4c50
Michael Ryan Bannon <ryan.bannon@gmail.com>
2016-05-25 15:05:25 -0400

    FEATURE: manual poly mask now gives and takes polygons
    This is realized as input/output ports. So, we can feed music staff results to a general poly mask job without having to put the poly mask interface into the wrappers for those jobs

--- wrappers/toolkits/custom/poly_mask.py
+++ wrappers/toolkits/custom/poly_mask.py
@@ -21,43 +21,70 @@ class PolyMask(RodanTask):
         'resource_types': ['image/onebit+png', 'image/greyscale+png', 'image/grey16+png', 'image/rgb+png'],
         'minimum': 1,
         'maximum': 1
+    },
+    {
+        'name': 'Polygons',
+        'resource_types': ['application/gamera-polygons+txt'],
+        'minimum': 0,
+        'maximum': 1
     }]
     output_port_types = [{
         'name': 'PNG image (masked)',
         'resource_types': ['image/onebit+png', 'image/greyscale+png', 'image/grey16+png', 'image/rgb+png'],
-        'minimum': 1,
+        'minimum': 0,
+        'maximum': 1
+    },
+    {
+        'name': 'Polygons',
+        'resource_types': ['application/gamera-polygons+txt'],
+        'minimum': 0,
         'maximum': 1
     }]
 
     def run_my_task(self, inputs, settings, outputs):
         if '@polygon_outer_points' not in settings:
             task_image = load_image(inputs['PNG image'][0]['resource_path'])
-            settings_update = {'@image_width': task_image.ncols}
+            polygon_outer_points = ''
+            if 'Polygons' in inputs:
+                with open(inputs['Polygons'][0]['resource_path'], 'r') as myfile:
+                    polygon_outer_points = myfile.read().replace('\n', '')
+            settings_update = {'@image_width': task_image.ncols, '@polygon_outer_points': polygon_outer_points}
             return self.WAITING_FOR_INPUT(settings_update)
         else:
-            task_image = load_image(inputs['PNG image'][0]['resource_path'])
-
-            mask_img = Image.new('L', (task_image.ncols, task_image.nrows), color='white')
-            mask_drawer = ImageDraw.Draw(mask_img)
-
             polygon_outer_points = settings['@polygon_outer_points']
-            for polygon in polygon_outer_points:
-                flattened_poly = [j for i in polygon for j in i]
-                mask_drawer.polygon(flattened_poly, outline='black', fill='black')
-            del mask_drawer
 
-            task_image_rgb = task_image.to_rgb()    # Because gamera masking doesn't work on onebit or grey16 images.
-            segment_mask = from_pil(mask_img).to_onebit()
-            result_image_rgb = task_image_rgb.mask(segment_mask)
-            result_image = ensure_pixel_type(result_image_rgb, outputs['PNG image (masked)'][0]['resource_type'])
+            # If image output port, write image
+            if 'PNG image (masked)' in outputs:
+                task_image = load_image(inputs['PNG image'][0]['resource_path'])
 
-            result_image.save_PNG(outputs['PNG image (masked)'][0]['resource_path'])
+                mask_img = Image.new('L', (task_image.ncols, task_image.nrows), color='white')
+                mask_drawer = ImageDraw.Draw(mask_img)
+
+                for polygon in polygon_outer_points:
+                    flattened_poly = [j for i in polygon for j in i]
+                    mask_drawer.polygon(flattened_poly, outline='black', fill='black')
+                del mask_drawer
+
+                task_image_rgb = task_image.to_rgb()    # Because gamera masking doesn't work on onebit or grey16 images.
+                segment_mask = from_pil(mask_img).to_onebit()
+                result_image_rgb = task_image_rgb.mask(segment_mask)
+                result_image = ensure_pixel_type(result_image_rgb, outputs['PNG image (masked)'][0]['resource_type'])
+
+                result_image.save_PNG(outputs['PNG image (masked)'][0]['resource_path'])
+
+            # If polygons image output port, write poly file
+            if 'Polygons' in outputs:
+                poly_string = str(polygon_outer_points)
+                f = open(outputs['Polygons'][0]['resource_path'], 'w')
+                f.write(poly_string)
+                f.close()
 
     def get_my_interface(self, inputs, settings):
         t = 'interfaces/poly_mask.html'
         c = {
             'image_url': inputs['PNG image'][0]['large_thumb_url'],
-            'image_width': settings['@image_width']
+            'image_width': settings['@image_width'],
+            'polygon_outer_points': settings['@polygon_outer_points']
         }
         return (t, c)
 
